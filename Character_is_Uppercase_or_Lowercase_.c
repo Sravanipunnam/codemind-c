064c04ac131859041be1981ea5d206fa85f0b952
Sravanipunnam <147476006+Sravanipunnam@users.noreply.github.com>
2023-10-15 11:58:38 +0530

    #include<stdio.h> int main(){     char ch;     scanf("%c",&ch);     if(ch>=65&&ch<=90)     {         printf("uppercase alphabet");     }     else if(ch>=97 &&ch<=122)     {         printf("lowercase alphabet");     }     else{         printf("not an alphabet");     } }

---
+++ Character_is_Uppercase_or_Lowercase_.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    scanf("%c",&ch);
+    if(ch>=65&&ch<=90)
+    {
+        printf("uppercase alphabet");
+    }
+    else if(ch>=97 &&ch<=122)
+    {
+        printf("lowercase alphabet");
+    }
+    else{
+        printf("not an alphabet");
+    }
+}
